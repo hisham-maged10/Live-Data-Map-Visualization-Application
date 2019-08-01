de6aa9a3dd20358f282cce696101e6d90f696745
Hisham.Maged22 <hisham.maged10@gmail.com>
2019-08-01 09:27:53 +0200

    Added Smooth Scrolling in site

--- js/script.js
+++ js/script.js
@@ -17,3 +17,12 @@ $(document).ready(function(){
   });
   
 })
+
+$(document).ready(function() {
+    $('a[href*=\\#]').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $(this.hash).offset().top
+        }, 750);
+    });
+});
